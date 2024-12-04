d119cc2b9ba542fc09f78f0bd1a89f724da5ad96
ROGUH Rivera Calzadillas <roguh@roguh.com>
2024-12-03 22:58:34 -0700

    naming

--- round1/portfolios.sh
+++ round1/portfolios.sh
@@ -13,37 +13,37 @@ SHARED_PARAMS="\
 
 ./gen.py $SHARED_PARAMS --custom-css '.__gallery{max-width:960px;}' --order-from-filter --filter-images \
     FEL180,FEL1746,FEL9793,FEL1738,14.52,FEL1709,FEL7638,FEL1817,FEL1835,FEL1834 \
-    > $OUTDIR/portfolio.html
+    > $OUTDIR/index.html
 
 echo
 
 ./gen.py $SHARED_PARAMS --order-from-filter --filter-images \
     FEL1978,FEL1976,15.14.00,FEL1464,FEL9793,FEL1997 \
-    > $OUTDIR/bw_portfolio.html
+    > $OUTDIR/bw.html
 
 echo
 
 ./gen.py $SHARED_PARAMS --order-from-filter --filter-images \
     _FEL0017,_FEL1189,_FEL1350,_FEL1356,_FEL1513,_FEL6640,_FEL7134,_FEL7483,_FEL7638,_FEL8017,_FEL9793,_FEL9888,_FEL9990_1,_FEL9994 \
-    > $OUTDIR/landscape_portfolio.html
+    > $OUTDIR/land.html
 
 echo
 
 ./gen.py $SHARED_PARAMS --order-from-filter --filter-images \
     _FEL5295_piloto_alerto,_FEL5326,_FEL6710,_FEL7798 \
-    > $OUTDIR/wildlife_portfolio.html
+    > $OUTDIR/life.html
 
 echo
 
 ./gen.py $SHARED_PARAMS --order-from-filter --filter-images \
     _FEL1513,_FEL6640,_FEL7134,_FEL7483 \
-    > $OUTDIR/lowlight_portfolio.html
+    > $OUTDIR/low.html
 
 echo
 
 ./gen.py $SHARED_PARAMS --order-from-filter --filter-images \
     FEL0970,FEL1011 \
-    > $OUTDIR/minimalist_portfolio.html
+    > $OUTDIR/minimal.html
 
 echo
 
